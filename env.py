58719960ef5967972b272aa3443784415e1e9207
enocol <eno_col@yahoo.com>
2025-05-30 03:35:43 +0100

    Edit styles

--- env.py
+++ env.py
@@ -4,5 +4,6 @@ import os
 
 URL = "postgresql://neondb_owner:npg_mTJOzXVn1kt4@ep-bitter-mud-a2g3b5ng.eu-central-1.aws.neon.tech/fox_dock_frill_839343"
 SECRET_KEY = 'django-insecure-q!_9sq!%n_oo-7_qq(0@rvhvhz!s2r$cl8!ny+sc3&zl5h_c*-'
+DEBUG = False
 os.environ.setdefault("DATABASE_URL", URL)
 os.environ.setdefault("SECRET_KEY", SECRET_KEY)
